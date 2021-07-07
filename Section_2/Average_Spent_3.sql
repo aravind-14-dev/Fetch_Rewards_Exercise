d3c7aaead03e55da7ca153f4ed025be9ae30f43e
Aravind14 <60626468+aravind-14-dev@users.noreply.github.com>
2021-07-07 14:54:54 -0500

    Delete Section_2 directory

--- Section_2/Average_Spent_3.sql
+++
@@ -1,9 +0,0 @@
-with table1 as
-(select rewardsReceiptStatus, totalSpent
-from receipts where rewardsReceiptStatus in ("FINISHED","REJECTED")),
-table2 as 
-(select rewardsReceiptStatus, avg(totalSpent) as average_spent
-from table1 group by rewardsReceiptStatus)
-
-select rewardsReceiptStatus, max(average_spend) as maximum_spent from table2;
-
